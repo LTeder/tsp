e5c0ee2723e55543db2f90b20976d8135168899e
Logan Teder <l.teder@columbia.edu>
2023-12-28 17:33:10 -0500

    Added 2 crossover methods
    Improved overall functionality

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # rust-tsp, "Rusty Genes"
-- A Rust implementation of a genetic program to solve the travelling salesperson problem
-- See [this repo](https://github.com/LTeder/rust-symboreg) for my other attempt at writing a genetic program / coding in Rust
+- A Rust implementation of a genetic program to solve the travelling salesperson problem.
+- The program reads a list of cities from a file, runs a genetic algorithm to find an optimal path, and prints the best path found.
+- The genetic algorithm uses a combination of single point, uniform order, and partially mapped crossover methods for breeding, and a simple swap mutation method.
+- The program uses command line arguments to control the number of iterations, population size, crossover rate, mutation rate, and survival rate.
+- See [this repo](https://github.com/LTeder/rust-symboreg) for my other attempt at writing a genetic program/Rust programming.
 
-Here I will experiment with AI pair-programming by using ChatGPT in [Cursor](https://www.cursor.so/).
-This way I hope to implement the visualization and performance graphing in Rust, where previously I used Python.
+## Next Steps
+- Add unit tests to ensure the correctness of the genetic algorithm.
+- Add additional mutation methods.
+- Implement a GUI visualization of the program as it runs using a library such as `iced`. It should display the current best path and update in real time as the program runs.
--- src/tsp.rs
+++ src/tsp.rs
@@ -1,10 +1,11 @@
 extern crate rand;
 
+use std::collections::{HashMap, HashSet};
+
 use self::rand::{Rng, thread_rng};
 use self::rand::seq::SliceRandom;
 use self::rand::distributions::{Distribution, Uniform};
 
-// Much of this is sourced from https://dev.to/d3spis3d/genetic-algorithm-in-rust-3gg
 pub struct City {
     pub x: f64,
     pub y: f64,
@@ -20,7 +21,7 @@ impl Path {
     pub fn calculate_fitness(path: &Vec<usize>, city_list: &Vec<City>) -> f64 {
         let path_length = city_list.len();
         let mut cost = 0.0;
-        for i in 0..path_length - 1 {
+        for i in 0..path_length - 2 {
             let a = &city_list[path[i]];
             let b = &city_list[path[i + 1]];
             cost = cost + ((a.x - b.x).powf(2.0) + (a.y - b.y).powf(2.0)).sqrt();
@@ -29,24 +30,86 @@ impl Path {
     }
     
     pub fn breed(&self, other: &Path, city_list: &Vec<City>) -> Path {
-        let order = Path::crossover_order(&self.order, &other.order);
+        let mut rng = thread_rng();
+        let crossover_type = Uniform::new(0, 3).sample(&mut rng);
+        let order = match crossover_type {
+            0 => Path::single_point_crossover(&self.order, &other.order),
+            1 => Path::uniform_order_crossover(&self.order, &other.order),
+            _ => Path::partially_mapped_crossover(&self.order, &other.order),
+        };
         let fitness = Path::calculate_fitness(&order, city_list);
         Path { fitness, order }
     }
-
-    fn crossover_order(mother: &Vec<usize>, father: &Vec<usize>) -> Vec<usize> {
+    
+    fn single_point_crossover(mother: &Vec<usize>, father: &Vec<usize>) -> Vec<usize> {
         let mut rng = thread_rng();
         let crossover_point = Uniform::new(0, mother.len()).sample(&mut rng);
         let mother_dna = &mother[0..crossover_point];
-        let mut father_dna: Vec<usize> = father.iter().filter_map(|d| {
+        let father_dna: Vec<usize> = father.iter().filter_map(|d| {
             if !mother_dna.contains(d) {
                 return Some(*d)
             }
             None
         }).collect();
-        let mut child = Vec::new();
-        child.extend_from_slice(mother_dna);
-        child.append(&mut father_dna);
+        let mut child = mother_dna.to_vec();
+        child.extend(father_dna);
+        child
+    }
+
+    fn uniform_order_crossover(mother: &Vec<usize>, father: &Vec<usize>) -> Vec<usize> {
+        let mut rng = thread_rng();
+        let mut child: Vec<usize> = vec![0; mother.len()];
+        let mut positions: Vec<usize> = (0..mother.len()).collect();
+        positions.shuffle(&mut rng);
+
+        // Copy a subset of gene positions from mother to child
+        for &position in positions.iter().take(mother.len() / 2) {
+            child[position] = mother[position];
+        }
+
+        // Fill the remaining positions with genes from father in the order they appear
+        let mut father_index = 0;
+        for position in 0..father.len() {
+            if child[position] == 0 {
+                while father_index < father.len() && child.contains(&father[father_index]) {
+                    father_index += 1;
+                }
+                if father_index < father.len() {
+                    child[position] = father[father_index];
+                }
+            }
+        }
+        child
+    }
+
+    fn partially_mapped_crossover(mother: &Vec<usize>, father: &Vec<usize>) -> Vec<usize> {
+        let mut rng = thread_rng();
+        let crossover_point1 = Uniform::new(0, mother.len()).sample(&mut rng);
+        let crossover_point2 = Uniform::new(0, mother.len()).sample(&mut rng);
+        let (start, end) = if crossover_point1 < crossover_point2 {
+            (crossover_point1, crossover_point2)
+        } else {
+            (crossover_point2, crossover_point1)
+        };
+    
+        let mut child: Vec<usize> = vec![0; mother.len()];
+        let mut gene_mapping: HashMap<usize, usize> = HashMap::new();
+        let mut gene_set: HashSet<usize> = HashSet::new();
+    
+        for i in start..end {
+            child[i] = mother[i];
+            gene_mapping.insert(mother[i], father[i]);
+            gene_set.insert(mother[i]);
+        }
+    
+        for i in (0..start).chain(end..mother.len()) {
+            let mut gene = father[i];
+            while gene_set.contains(&gene) {
+                gene = *gene_mapping.get(&gene).unwrap_or(&gene);
+            }
+            child[i] = gene;
+            gene_set.insert(gene);
+        }
         child
     }
 
@@ -90,11 +153,11 @@ impl Simulation {
 
     fn initial_population(city_list: &Vec<City>, population_size: usize) -> Vec<Path> {
         let base_list: Vec<usize> = (0..city_list.len()).collect();
+        let mut rng = thread_rng();
         let mut population: Vec<Path> = Vec::new();
     
         for _ in 0..population_size {
             let mut p = base_list.clone();
-            let mut rng = thread_rng();
             p.shuffle(&mut rng);
             let fitness = Path::calculate_fitness(&p, city_list);
     
@@ -105,7 +168,7 @@ impl Simulation {
     
     pub fn run(&mut self) -> () {
         let mut fittest = self.find_fittest();
-        println!("starting iterations");
+        println!("Starting {} iterations...", self.iterations);
         for _ in 0..self.iterations {
             self.generate_children();
             let challenger = self.find_fittest();
@@ -113,7 +176,7 @@ impl Simulation {
                 fittest = challenger;
             }
         }
-        println!("{:?}", fittest);
+        println!("Champion:\n{:?}", fittest);
     }
 
     fn find_fittest(&self) -> Path {
